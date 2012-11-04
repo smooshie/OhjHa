a6dfa1841e7eaafbc6f919fdaa34be29008e14a3
unknown <lisa.gawriyski@gmail.com>
2012-11-04 22:22:47 +0200

    JUnit Tests

---
+++ OhjHa/test/OhjHaTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import ohjha.FileHandling;
+import ohjha.OhjHa;
+import ohjha.Toiminto;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Smoosh
+ */
+public class OhjHaTest {
+    OhjHa ohjha;
+    Toiminto toiminto;
+    FileHandling tiedosto;
+    
+    public OhjHaTest() {   
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        OhjHa ohjha = new OhjHa();
+        Toiminto toiminto = new Toiminto();
+        FileHandling tiedosto = new FileHandling();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() {}
+    
+    @Test
+    public void PrintablePrints() {
+        String output = null;
+        String input = "Print!";
+        tiedosto.Printable(input, output);
+    }
+    
+    @Test
+    public void PrintableSaves() {
+        String output = "Test.txt";
+        String input = "In file.";
+        tiedosto.Printable(input, output);        
+    }
+    
+    @Test
+    public void ReadFromFile() {
+        String test;
+        String filename = "Test.txt";
+        test = tiedosto.ReadFrom(filename);
+        System.out.println(test);
+    }
+    
+}
